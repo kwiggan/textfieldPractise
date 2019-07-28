51857cde742fca7f0577f9513aa9d7a56535c33d
Keneisha Wiggan <keneishawiggan8@gmail.com>
2019-07-28 15:37:50 -0700

    create a stack view for textfield

--- TextFieldPractise/TextFieldPractise/ViewController.swift
+++ TextFieldPractise/TextFieldPractise/ViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
